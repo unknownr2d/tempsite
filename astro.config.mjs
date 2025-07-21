c94e4d2aad9cafca9694f76e290dfc3854dde1c3
Sahan Premarathna <sahanpremarathna2005@gmail.com>
2025-07-21 16:53:06 +0530

    hh

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tempsitee.netlify.app', // your Netlify site root
-  base: '/index2/',                      // your subdirectory (very important)
 });
